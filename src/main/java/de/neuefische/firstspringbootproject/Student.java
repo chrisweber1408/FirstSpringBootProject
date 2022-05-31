1e4d4f7c7cfb8b741eb70fc1d96a0aa4c085d942
chris <chrisweber1991@gmail.com>
2022-05-31 14:53:25 +0200

    Test

--- src/main/java/de/neuefische/firstspringbootproject/Student.java
+++ src/main/java/de/neuefische/firstspringbootproject/Student.java
@@ -2,16 +2,15 @@ package de.neuefische.firstspringbootproject;
 
 public class Student {
 
-    private String firstName;
-    private String lastName;
-    private int age;
+    private final String firstName;
+    private final String lastName;
+    private final int age;
 
     public Student(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
     }
-
     public String getFirstName() {
         return firstName;
     }
